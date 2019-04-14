a6615653bff6e2d050ffd2ae7250481cf87bacf5
Paulo <44127888+paulopazb@users.noreply.github.com>
2019-04-14 16:15:00 -0400

    Add files via upload

---
+++ Ejercicio 1.cpp
@@ -0,0 +1,27 @@
+// Ejercicio 1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include "NATURALES.h"
+
+using namespace std;
+
+
+
+int main()
+{
+	NATURALES a,b;
+	a.Leer();
+	b=a.Eliminar_repetidos();
+	b.Mostrar();
+
+
+	
+
+
+
+
+system("pause>null");
+return 0;
+}
